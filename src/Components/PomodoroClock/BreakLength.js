c118d420d76b4a833dd4d7e716422a750c9e9744
ArslanArshad105 <60978049+ArslanArshad105@users.noreply.github.com>
2023-06-19 18:15:36 +0500

    wrote function for Increase and Decrease value

--- src/Components/PomodoroClock/BreakLength.js
+++ src/Components/PomodoroClock/BreakLength.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Button } from "react-bootstrap";
 
 const BreakLength = () => {
+  const [increased, setIncreased] = useState(5);
+
+  const increasedValue = () => {
+    setIncreased(increased + 1);
+  };
+
+  const decreasedValue = () => {
+    if (increased > 1) {
+      setIncreased(increased - 1);
+    }
+  };
+
   return (
     <Container>
       <h3 className="Heading-Color">Break Length</h3>
-      <Button></Button>
-      <h4>5</h4>
-      <Button></Button>
+      <Button onClick={decreasedValue}>-</Button>
+      <h4 className="Heading4-width">{increased}</h4>
+      <Button onClick={increasedValue}>+</Button>
     </Container>
   );
 };
--- src/Components/PomodoroClock/SessionLength.js
+++ src/Components/PomodoroClock/SessionLength.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Button } from "react-bootstrap";
 
 const SessionLength = () => {
+  const [increased, setIncreased] = useState(25);
+
+  const increasedValue = () => {
+    setIncreased(increased + 1);
+  };
+
+  const decreasedValue = () => {
+    if (increased > 1) {
+      setIncreased(increased - 1);
+    }
+  };
+
   return (
     <Container>
       <h3 className="Heading-Color">Session Length</h3>
-      <Button></Button>
-      <h4>25</h4>
-      <Button></Button>
+      <Button onClick={decreasedValue}>-</Button>
+      <h4 className="Heading4-width">{increased}</h4>
+      <Button onClick={increasedValue}>+</Button>
     </Container>
   );
 };
