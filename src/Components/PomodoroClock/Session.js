a248c226e30a7ed312c49d805fecd28dcd646114
ArslanArshad105 <60978049+ArslanArshad105@users.noreply.github.com>
2023-06-21 15:19:35 +0500

    Customize Break and Length Code into one component

--- src/Components/PomodoroClock/Session.js
+++ src/Components/PomodoroClock/Session.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button, Col, Container } from "react-bootstrap";
 
-const Session = () => {
+const Session = (prop) => {
   return (
     <Container>
       <Col></Col>
       <Col className="Session-Col">
         <h3 className="Session-Heading">Session</h3>
-        <h4 className="Session-Heading">25</h4>
+        <h4 className="Session-Heading">{prop.timer}</h4>
       </Col>
       <Col></Col>
       <Button className="Session-Button">Start</Button>
