37c24e89a77adeb40b7a6ed16ecc30feda0ab9c6
ArslanArshad105 <60978049+ArslanArshad105@users.noreply.github.com>
2023-06-27 17:53:12 +0500

    Switching session to break

--- src/Components/PomodoroClock/Session.js
+++ src/Components/PomodoroClock/Session.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Button, Col, Container } from "react-bootstrap";
 
 const Session = (prop) => {
-  const { timer, resetBreak, resetTime } = prop;
+  const { breakTime, timer, resetBreak, resetTime } = prop;
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(timer);
   const [startTimer, setStartTimer] = useState();
+  const [isSessionTime, setIsSessionTime] = useState(false);
 
   useEffect(() => {
     setMinutes(timer);
@@ -13,20 +14,28 @@ const Session = (prop) => {
 
   useEffect(() => {
     if (startTimer) {
-      const interval = setInterval(() => {
+      const interval = setTimeout(() => {
         if (seconds > 0) {
           setSeconds((seconds) => seconds - 1);
         } else if (seconds === 0 && minutes > 0) {
           setMinutes((minutes) => minutes - 1);
           setSeconds(59);
+        } else if (seconds === 0 && minutes === 0) {
+          if (isSessionTime === true) {
+            setMinutes(() => timer);
+          } else {
+            setMinutes(() => breakTime);
+          }
+          setIsSessionTime(!isSessionTime);
         }
-      }, 1000);
+      }, 100);
       return () => clearInterval(interval);
     }
   }, [seconds, minutes, startTimer]);
 
   const onClickReset = () => {
     setStartTimer(false);
+    setIsSessionTime(false);
     resetBreak(5);
     resetTime(25);
     setMinutes(25);
@@ -36,7 +45,9 @@ const Session = (prop) => {
     <Container>
       <Col></Col>
       <Col className="Session-Col">
-        <h3 className="Session-Heading">Session</h3>
+        <h3 className="Session-Heading">
+          {isSessionTime === false ? <span>Session</span> : <span>Break</span>}
+        </h3>
         <h4 className="Session-Heading">
           {minutes > 9 ? minutes : <span>0{minutes}</span>}:
           {seconds > 9 ? seconds : <span>0{seconds}</span>}
